c6c222e7afd82a51c439b37a5acd0be2d83a9145
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-11 15:17:38 +0100

    Create MainModelClass.swift

---
+++ MainModelClass.swift
@@ -0,0 +1,71 @@
+import Foundation
+struct Concentration
+
+{
+    
+    
+private(set) var cards = [Card]()
+    
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+          
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                        
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+                
+            }
+        }
+
+    }
+    
+
+mutating func chooseCard(at index: Int) {
+    assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
+    if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex] == cards[index] {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+             
+            } else {
+                
+                indexOfOneAndOnlyFaceUpCard = index
+                }
+                
+            }
+            
+        }
+
+
+    init(numberOfPairsOfCards: Int){
+         assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must at least have one pair of cards")
+        for _ in 0...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+            
+    }
+
+}
+}
+
+    extension Collection {
+        var oneAndOnly: Element? {
+            return count == 1 ? first : nil
+        }
+    }
